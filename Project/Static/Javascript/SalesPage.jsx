41944cc69a93eaf5e5f80042a1b1f9f974353218
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-05-22 22:07:51 -0500

    Add hotkeys

--- Project/Static/Javascript/SalesPage.jsx
+++ Project/Static/Javascript/SalesPage.jsx
@@ -4,19 +4,28 @@ import {HotKeys} from "react-hotkeys";
 
 
 
-// =====================================================================
-// ============           SALES PAGE       =============================
-// =====================================================================
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// ||||||||||||           SALES PAGE       |||||||||||||||||||||||||||||
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 export default class SalesPage extends React.Component {
 
+
+    // =========================================================
+    // ========        CONSTRUCTOR AND STATE         ===========
+    // =========================================================
     constructor(props) {
         super(props)
 
         this.state = {
+            FocusGroup: {
+                CurrentFocus : 1,
+                FocusElements: ["QuantityOrPriceInput", "BarCodeInput", "SearchInput"]
+            },
             ToPay: 0.0,
             CurrentSell: {
                 QuantityOrPrice: "1",
-                BarCode: "",
+                BarCode: " ",
+                Search: " "
             },
             Products: [
               {
@@ -35,37 +44,75 @@ export default class SalesPage extends React.Component {
             HotKeysMapping: {
                 keyMap: {
                     SendProduct: 'enter',
+                    MoveToRight: ['shift+right', 'alt+right', 'ctrl+right'],
+                    MoveToLeft: ['shift+left', 'alt+left', 'ctrl+left'],
+                    SetFocusToQuantityOrPrice: ['|'],
+                    SetFocusToBarCode: ['@'],
+                    SetFocusToSearch: ['#'],
                 },
                 handlers: {
-                    'SendProduct': (event) => {
-                        const Sell = this.state.CurrentSell
-                        const IsPrice = Sell.QuantityOrPrice[0] === '$'
-                        Sell.QuantityOrPrice = Sell.QuantityOrPrice.slice(IsPrice? 1: 0)
-
-                        if (Number(Sell.QuantityOrPrice) == NaN) {
-                            alert("Error en el precio")
-                            return
-                        }
-
-                        console.log(`Enviamos ${(IsPrice)? "$" : ""} ${Sell.QuantityOrPrice} Code bar ${Sell.BarCode}`)
-                    }
+                    'SendProduct': (e) => this.handleSendProduct(),
+                    'MoveToRight': (e) => this.handleChangeOfFocus("right", null),
+                    'MoveToLeft':  (e) => this.handleChangeOfFocus("left", null),
+                    'SetFocusToQuantityOrPrice':  (e) => this.handleChangeOfFocus(null, 0),
+                    'SetFocusToBarCode':          (e) => this.handleChangeOfFocus(null, 1),
+                    'SetFocusToSearch':           (e) => this.handleChangeOfFocus(null, 2),
                 }
-            }
+            } 
         }
     }
 
-    handleChangeQuantityOrPrice = (Event) => {
+
+    // =========================================================
+    // ===========              HANDLES            =============
+    // =========================================================
+    handleCurrentSaleData (Event, Item) {
         const NewCurrentSell = Object.assign({}, this.state.CurrentSell)
-        NewCurrentSell.QuantityOrPrice = Event.target.value
-        this.setState({CurrentSell: NewCurrentSell})
+        const NewFocusGroup = Object.assign({}, this.state.FocusGroup)
+        NewCurrentSell[Item] = Event.target.value
+        NewFocusGroup.CurrentFocus = NewFocusGroup.FocusElements.findIndex(name => name === Item + "Input")
+
+        this.setState({CurrentSell: NewCurrentSell, FocusGroup: NewFocusGroup})
     }
 
-    handleChangeBarCode = (Event) => {
-        const NewCurrentSell = Object.assign({}, this.state.CurrentSell)
-        NewCurrentSell.BarCode = Event.target.value
-        this.setState({CurrentSell: NewCurrentSell})
+    handleChangeOfFocus (Direction, Position) {
+        const NewFocusGroup = this.state.FocusGroup
+        let NewCurrentFocus = 1
+
+        if (Direction == null) {
+            NewCurrentFocus = Position
+        }
+        else {
+            const Move = (Direction === "right")? 1: -1;
+            NewCurrentFocus = (NewFocusGroup.CurrentFocus + Move) % 3
+            if (NewCurrentFocus < 0) NewCurrentFocus += 3
+        }
+
+        NewFocusGroup.CurrentFocus = NewCurrentFocus
+        this.setState({FocusGroup: NewFocusGroup})
+
+        document.getElementById(NewFocusGroup.FocusElements[NewFocusGroup.CurrentFocus]).focus()
     }
 
+    handleSendProduct () {
+        const Sell = this.state.CurrentSell
+        const IsPrice = Sell.QuantityOrPrice[0] === '$'
+        Sell.QuantityOrPrice = Sell.QuantityOrPrice.slice(IsPrice? 1: 0)
+
+        if (Number(Sell.QuantityOrPrice) == NaN) {
+            alert("Error en el precio")
+            return
+        }
+
+        if (IsPrice) console.log(`Send Price ${Sell.QuantityOrPrice} Code Bar: ${Sell.BarCode}`)
+        else console.log(`Send ${Sell.QuantityOrPrice} of Code Bar: ${Sell.BarCode}`)
+
+    }
+
+
+    // =========================================================
+    // ============           RENDER              ==============
+    // =========================================================
     render () {
 
         const TableProductsItems = this.state.Products.map( (Product) => {
@@ -104,48 +151,64 @@ export default class SalesPage extends React.Component {
                 {/*=====================================================*/}
                 <div className="divider" />
                 <div className="section">
+
                     <HotKeys keyMap={this.state.HotKeysMapping.keyMap} handlers={this.state.HotKeysMapping.handlers}>
                     <div className="row">
                       
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
+                        {/*+++++++++    QUANTITY OR PRICE INPUT     ++++++++++++*/}
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
                         <div className="input-field col s2">
                             <input 
-                                id        = "QuantityOrPrice" 
+                                id        = "QuantityOrPriceInput" 
                                 type      = "text"
-                                className = "validate"
                                 value     = {this.state.CurrentSell.QuantityOrPrice}
-                                onChange  = {this.handleChangeQuantityOrPrice}
+                                onFocus   = {() => this.handleChangeOfFocus(null, 0)}
+                                onChange  = {(e) => this.handleCurrentSaleData(e, "QuantityOrPrice")}
                             />
-                            <label htmlFor="QuantityOrPrice">
+                            <label htmlFor="QuantityOrPriceInput">
                                 <div style={{fontSize: '0.8em'}}>
                                     Cantidad o Precio
                                 </div>
                             </label>
                         </div>
 
-                        <div className="input-field col s5 focus">
-                            <input 
-                                id    = "CodeInput" 
-                                type  = "text"
-                                value = {this.state.CurrentSell.BarCode}
-                                onChange = {this.handleChangeBarCode}
-                            />
-                            <label htmlFor="CodeInput">Código de Barras del Producto</label>
-                        </div>
-
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
+                        {/*+++++++++          BAR CODE INPUT        ++++++++++++*/}
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
                         <div className="input-field col s5">
                             <input 
-                                id = "SearchInput" 
-                                type = "text"
+                                id        = "BarCodeInput" 
+                                type      = "text"
+                                autoFocus = {true}
+                                value     = {this.state.CurrentSell.BarCode}
+                                onFocus   = {() => this.handleChangeOfFocus(null, 1)}
+                                onChange  = {(e) => this.handleCurrentSaleData(e, "BarCode")}
+                            />
+                            <label htmlFor="BarCodeInput">Código de Barras del Producto</label>
+                        </div>
+
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
+                        {/*+++++++++         SEARCH INPUT           ++++++++++++*/}
+                        {/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
+                        <div className="input-field col s5">
+                            <input 
+                                id        = "SearchInput" 
+                                type      = "text"
+                                value     = {this.state.CurrentSell.Search}
+                                onFocus   = {() => this.handleChangeOfFocus(null, 2)}
+                                onChange  = {(e) => this.handleCurrentSaleData(e, "Search")}
                             />
                             <label htmlFor="SearchInput">Buscar por Nombre</label>
                         </div>
+
                     </div>
                     </HotKeys>
                 </div>
 
 
                 {/*=====================================================*/}
-                {/*=========          PRODUCTS TABLES       ============*/}
+                {/*=========          PRODUCTS TABLE        ============*/}
                 {/*=====================================================*/}
                 <div className="divider" />
                 <div className="row section">
--- README.md
+++ README.md
@@ -19,3 +19,10 @@
 * Flask
 
 
+
+## Characteristics
+
+### Sales Page
+- Autofocus in codebar
+- If you put `$` then it will be render as a price, for example, 
+`$10` means $10 of an item, like $10 of eggs and `10` is 10 items
